3041a12c0ce2501d70ffe0b2dde2d1bf83cb4e06
EliasWagnerdev <eliasvagner.456@gmail.com>
2022-09-21 02:18:06 -0300

    version 1.1

--- js/script.js
+++ js/script.js
@@ -3,10 +3,10 @@ let textoArray = document.querySelector('.digitar');
 function ativa() {
     if (menu.classList.contains('ativar')) {
         menu.classList.remove('ativar');
-        document.querySelector('.menuMobile').src = "/img/Menu.svg";
+        document.querySelector('.menuMobile').src = "img/Menu.svg";
     } else {
         menu.classList.add('ativar');
-        document.querySelector('.menuMobile').src = "/img/close.svg";
+        document.querySelector('.menuMobile').src = "img/close.svg";
     }
 }
 
