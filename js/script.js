7d01c32564251be2dbb02506ccfb247ce5b38bae
EliasWagnerdev <eliasvagner.456@gmail.com>
2022-09-21 13:00:27 -0300

    version 1.2

--- js/script.js
+++ js/script.js
@@ -1,18 +1,27 @@
 let menu = document.querySelector('.canvas');
-let textoArray = document.querySelector('.digitar');
+let p = document.querySelector('.digitar');
+let p2 = document.querySelector('#ew');
+let digita2 = document.querySelector('.digitarp');
+
 function ativa() {
     if (menu.classList.contains('ativar')) {
         menu.classList.remove('ativar');
         document.querySelector('.menuMobile').src="img/Menu.svg";
+        digita2.classList.remove('opa');
+        p.classList.remove('opa');
+        p2.classList.remove('opa');
     } else {
         menu.classList.add('ativar');
         document.querySelector('.menuMobile').src="img/Close.svg";
+        digita2.classList.add('opa');
+        p.classList.add('opa');
+        p2.classList.add('opa');
     }
 }
 
 
-const secundo = 'Portifolio';
-let digita2 = document.querySelector('.digitarp');
+const secundo = 'Portifolio.';
+
 
 
 for (let index = 0; index < (secundo.length); index++) {
@@ -20,7 +29,7 @@ for (let index = 0; index < (secundo.length); index++) {
         (function (pos) {
             setTimeout(function () {
                 digita2.innerHTML += secundo.charAt(pos);
-            }, 400 * pos);
+            }, 200 * pos);
         })(index);
     
 
