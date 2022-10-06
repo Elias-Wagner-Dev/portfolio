d587d51c8f2cc734be86b3604cec0b24e5b86876
EliasWagnerdev <eliasvagner.456@gmail.com>
2022-10-06 16:25:26 -0300

    version 1.6

--- js/animacao.js
+++ js/animacao.js
@@ -3,15 +3,5 @@ let cardsAdd = document.querySelector('.cardAdd');
 let tt = document.getElementById('tt');
 
 function mostra(){
-    console.log('mostra');
-    if(cards.classList.contains('mostra')){
-        cards.classList.remove('mostra');
-        cardsAdd.classList.remove('cardAddMostra');
-        tt.classList.remove('cardAddMostra');
-
-    }else{
-        cards.classList.add('mostra');
-        cardsAdd.classList.add('cardAddMostra');
-        tt.classList.add('cardAddMostra')
-    }
+    alert('Sem Projetos Por em quanto');
 }
